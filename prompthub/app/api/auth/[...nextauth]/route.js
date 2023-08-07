880f8ff69e4e07e1348beeff3f39e66bfa7dcc4f
Anjali <anjalisah89@gmail.com>
2023-08-07 20:56:25 +0530

    Creating prompt feature and connected to MongoDB

--- prompthub/app/api/auth/[...nextauth]/route.js
+++ prompthub/app/api/auth/[...nextauth]/route.js
@@ -10,47 +10,50 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    callbacks:{
+        async session({ session }){
+            // find existing users
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
     
-    async session({ session }){
-        // find existing users
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        //updating the data of an existing users
-        session.user.id = sessionUser._id.toString();
-
-        return session;
-    },
-
-    async signIn({ profile }){
-       
-        try{
-            //serverless -> dynamicdb
-            await connectToDB();
-            // check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
-
-            //if not, create a new user
-
-            if(!userExists){
-                await User.create({
-                    email: profile.email,
-                    username:profile.name.replace(" ","").toLowerCase(),
-                    image: profile.picture
-                })
+            //updating the data of an existing users
+            session.user.id = sessionUser._id.toString();
+    
+            return session;
+        },
+    
+        async signIn({ profile }){
+           
+            try{
+                //serverless -> dynamicdb
+                await connectToDB();
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+    
+                //if not, create a new user
+    
+                if(!userExists){
+                    await User.create({
+                        email: profile.email,
+                        username:profile.name.replaceAll(/\s/g,"").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+    
+                return true;
             }
-
-            return true;
+            catch(error){
+                console.log(error);
+                return false;
+            }
+    
         }
-        catch(error){
-            console.log(error);
-            return false;
-        }
-
     }
+    
+   
 })
 
 export { handler as GET, handler as POST };
--- prompthub/components/Form.jsx
+++ prompthub/components/Form.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import Link from 'next/link';
 
-const Form = () => {
+const Form = ({
+  type,
+  post,
+  setPost,
+  submitting,
+  handleSubmit
+}) => {
   return (
-    <div>Form</div>
+    <section className=' w-full max-w-full flex-center flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span></h1>
+        <p className='desc text-left max-w-md'> {type} & Share amazing prompts with others and personalize your feed based on your own interests. Unleash your creativity while discovering the diverse minds of others with any AI-powered platform. </p>
+        
+        <form onSubmit={handleSubmit} className='mt-10 w-full max-w-2xl flex-col gap-7 glassmorphism'> 
+        <label>
+          <span className='font-satoshi font-semibold text-base text-grey-700'> Your AI Prompt</span>
+          <textarea value={post.prompt} onChange={(e) => setPost( {...post,prompt: e.target.value})} placeholder='Write your prompt here...' required className='form_textarea'/>
+        </label>
+        <br/>
+        <label> 
+          <span className='font-satoshi font-semibold text-base text-gray-700'> Tags <span className='font-normal ml-2'>(#product, #webdevelopment, #idea #myprompt)</span>
+          </span>
+          <input value={post.tag} onChange={(e) => setPost( {...post,tag: e.target.value})} placeholder='#tags' className='form_input'/>
+        </label>
+        
+        <div className='flex-end mx-3 pt-5 mb-5 gap-4'>
+          <Link href="/" className='text-gray-500'>Cancel</Link>
+          <button type='submit' disabled={submitting} className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'>{submitting? `${type}...` : type}</button>
+          </div>
+      </form>
+    </section>
   )
 }
 

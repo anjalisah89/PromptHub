c162ecab08ad6828c857d1d6176b5e30082852a8
Anjali <anjalisah89@gmail.com>
2023-08-26 12:28:07 +0530

    Adding Features

--- prompthub/components/Form.jsx
+++ prompthub/components/Form.jsx
@@ -1,9 +1,4 @@
-
-'use client'
-
 import Link from 'next/link';
-import Image from 'next/image';
-
 
 //Form Layout
 const Form = ({
@@ -17,25 +12,7 @@ const Form = ({
   return (
     // Form layout used in Create post and Edit post
     <section className=' w-full max-w-full flex-center flex-col'>
-      <div>
-      <h1 className='head_text text-center'>Summarize Articles with
-      <br/>
-      <span className='orange_gradient'>OpenAI GPT-4</span>     
-      </h1>
-      <p className='desc text-center pt-3'>Improved your articles by generating coherent and contextually relevant text across diverse topics & prompts and share on you feed.</p>
 
-      <div>
-        <form className="relative flex justify-center item-center mt-5" onSubmit={() =>{}}>
-          <Image src={'/assets/icons/link.svg'} width={12} height={12} alt="link_icon" className=" absolute left-0 pt-1 my-2 ml-3 w-5"/>
-          <input type='url' placeholder='Enter a url or text' value={() =>{}} onChange={() =>{}} className='url_input peer' required/>
-          <button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'>↵</button>
-        </form>
-
-      </div>
-    </div>
-    <br/>
-      
-     
       <h1 className='head_text'>
         <span className='blue_gradient'>{type} Post</span></h1>
         <p className='desc text-left max-w-md'> {type} & Share amazing prompts with others and personalize your feeds based on your own interests. Unleash your creativity while discovering the diverse minds of others with any AI-powered platform. </p>
